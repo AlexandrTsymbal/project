454f2edc41da89207e1d2b7381ff4fb2e7f59c0e
Lenkeri <lno4kamalt@yandex.ru>
2021-04-19 19:47:17 +0500

    init

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ImageView image = findViewById(R.id.imageView2);
+        super.onCreate(savedInstanceState);
+
+    }
