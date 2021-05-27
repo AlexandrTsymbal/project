50cbee49bf88dc8b529353dc863b5bf296aa4257
haschish <haschish@gmail.com>
2021-05-27 14:41:44 +0500

    added simple final dialog

--- app/src/main/java/com/example/project/GameFragment.java
+++ app/src/main/java/com/example/project/GameFragment.java
@@ -15,22 +15,50 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.project.databinding.GameFragmentBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class GameFragment extends Fragment {
     GameViewModel viewModel;
+    GameFragmentBinding binding;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        GameFragmentBinding binding = GameFragmentBinding.inflate(getLayoutInflater());
+        binding = GameFragmentBinding.inflate(getLayoutInflater());
         viewModel = new ViewModelProvider(this).get(GameViewModel.class);
+        viewModel.getStatus().observe(getViewLifecycleOwner(), (GameStatus status) -> {
+            if (status == GameStatus.END) {
+                showFinalDialog();
+            }
+        });
 
         binding.setLifecycleOwner(this);
         binding.setViewModel(viewModel);
         return binding.getRoot();
+    }
+
+    public void showFinalDialog() {
+        new MaterialAlertDialogBuilder(getContext())
+                .setTitle("Congratulations!")
+                .setMessage("You know {x} celebrities")
+                .setCancelable(false)
+                .setNegativeButton("exit", (i, v) -> exitGame())
+                .setPositiveButton("play again", (i, v) -> restartGame())
+                .show();
+    }
+
+    private void exitGame() {
+        NavController navController = Navigation.findNavController(binding.getRoot());
+        navController.navigateUp();
+    }
+
+    private void restartGame() {
+        viewModel.start();
     }
 }
 
